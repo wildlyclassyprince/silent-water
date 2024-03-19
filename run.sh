e1787ecb62b5aa9d11b78200d62f8606b155b76f
wildlyclassyprince <wildlyclassyprince@gmail.com>
2024-03-19 17:00:10 +0100

    simplify setup steps

---
+++ run.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Create a virtual environment and download requirements
+echo "Creating virtual environment in $PWD"
+python3 -m venv venv && . venv/bin/activate
+
+echo "Installing requirements ..."
+pip install -U pip -r requirements.txt
+echo "DONE"
+
+# Setup dbt project
+echo "Setting up dbt project ..."
+pushd dbt
+dbt clean && dbt deps && dbt compile --profiles-dir ./
+popd
+echo "DONE"
+
+# Run Dagster ...
+echo "Starting Dagster ..."
+pushd dwh
+dagster dev
