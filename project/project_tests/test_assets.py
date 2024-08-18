1f686a1a2295fa86ff349e2e4fc11dd78f7f2207
wildlyclassyprince <mthuli.bhebhe@gmail.com>
2024-08-18 15:34:25 +0200

    initial commit - add asset tests

--- project/project_tests/test_assets.py
+++ project/project_tests/test_assets.py
@@ -1,1 +1,7 @@
+from dwh.raw.assets import income_age_and_sex
 
+def test_income_age_and_sex():
+    """
+    Test the function that fetches OECD income and sex data and writes it to DuckDB.
+    """
+    raise NotImplementedError
